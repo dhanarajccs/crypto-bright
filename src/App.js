66b5d988b97cc1a27b15ab142d89a013d07193e2
dhanarajccs <dhanarajccs@gmail.com>
2023-04-04 13:30:02 +0530

    import useState, useEffect, axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data);
+      console.log(response.data);
+    }).catch((error) => {
+      console.log(error);
+    })
+  }, [])
+  
   return (
     <>
       CryptoBright App
