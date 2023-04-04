253334bec048add67eb9edbc8550f488160b7c46
dhanarajccs <dhanarajccs@gmail.com>
2023-04-04 14:11:34 +0530

    pass coins as props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Coins from './components/Coins';
 
 function App() {
 
@@ -18,7 +19,7 @@ function App() {
   
   return (
     <>
-      CryptoBright App
+      <Coins coins={coins} />
     </>
   );
 }
