ec8077ddaabfe3bd9b007a2ad94794356e7138ad
dhanarajccs <dhanarajccs@gmail.com>
2023-04-04 13:37:59 +0530

    get API url

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
 
   const [coins, setCoins] = useState([]);
 
+  const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en`
+
   useEffect(() => {
     axios.get(url).then((response) => {
       setCoins(response.data);
