fba6ae22ef35654a72fbbb8a423802674505d16c
dhanarajccs <dhanarajccs@gmail.com>
2023-04-04 14:12:59 +0530

    Coins component

---
+++ src/components/Coins.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Coins = (props) => {
+  return (
+    <div>
+      <div>
+        <div>#</div>
+        <div>Coin</div>
+        <div>Price</div>
+        <div>24h</div>
+        <div>Volume</div>
+        <div>Market Cap</div>
+      </div>
+    </div>
+  )
+}
+
+export default Coins
